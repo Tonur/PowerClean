95ee74598a8510d72f53267184cee961bfebf70c
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-10-03 16:16:55 +0200

    Changed the vsct to use the template described in the top of the vsct file. Now acts as a button on the Solution Explorer => Solution Context.

--- src/PowerCleanCore/PowerCleanCorePackage.cs
+++ src/PowerCleanCore/PowerCleanCorePackage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
@@ -6,50 +7,24 @@ using Task = System.Threading.Tasks.Task;
 
 namespace PowerCleanCore
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [Guid(PowerCleanCorePackage.PackageGuidString)]
-    [ProvideMenuResource("Menus.ctmenu", 1)]
-    public sealed class PowerCleanCorePackage : AsyncPackage
+  [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+  [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+  [Guid(PowerCleanCorePackage.PackageGuidString)]
+  [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+  [ProvideMenuResource("Menus.ctmenu", 1)]
+  public sealed class PowerCleanCorePackage : AsyncPackage
+  {
+    public const string PackageGuidString = "cffa6255-844b-403b-9a39-0361b700844d";
+
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
     {
-        /// <summary>
-        /// PowerCleanCorePackage GUID string.
-        /// </summary>
-        public const string PackageGuidString = "5308c29b-f784-4957-86de-fc2f6b057d44";
+      await base.InitializeAsync(cancellationToken, progress);
 
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        {
-            // When initialized asynchronously, the current thread may be a background thread at this point.
-            // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            await PowerShellCommand.InitializeAsync(this);
-        }
-
-        #endregion
+      // When initialized asynchronously, we *may* be on a background thread at this point.
+      // Do any initialization that requires the UI thread after switching to the UI thread.
+      // Otherwise, remove the switch to the UI thread if you don't need it.
+      await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+      await PowerShellCommand.InitializeAsync(this);
     }
+  }
 }
