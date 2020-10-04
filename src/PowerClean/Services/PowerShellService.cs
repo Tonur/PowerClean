b4faf40a0e3fe107a38be648004b99904a8d1fe9
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-10-04 03:01:21 +0200

    Powershell command added

--- src/PowerClean/Services/PowerShellService.cs
+++ src/PowerClean/Services/PowerShellService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Management.Automation;
+using System.Threading.Tasks;
 using PowerClean.Interfaces;
 
 namespace PowerClean.Services
@@ -6,12 +7,22 @@ namespace PowerClean.Services
 #nullable enable
   public class PowerShellService : IPowerShellService
   {
-    public void PowerClean(string folder) =>
-        PowerCleanAsync(folder).RunSynchronously(TaskScheduler.Default);
+    private static string PowerCleanCommand(string folder) => string.Concat($"Get-ChildItem {folder}", @"-include bin,obj -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }");
+
+    public void PowerClean(string folder)
+    {
+      var ps = PowerShell.Create();
+      ps.AddCommand(PowerCleanCommand(folder));
+
+      ps.Invoke();
+    }
 
     public async Task PowerCleanAsync(string folder)
     {
-      await Task.Delay(5000);
+      var ps = PowerShell.Create();
+      ps.AddCommand(PowerCleanCommand(folder));
+
+      await Task.FromResult(ps.BeginInvoke());
     }
   }
 }
